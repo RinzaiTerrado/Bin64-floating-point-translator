6498a2729ea943d90566f8991d7d55efb7194b16
RinzaiTerrado <75122826+RinzaiTerrado@users.noreply.github.com>
2022-02-09 22:11:30 +0800

    Add files via upload

---
+++ csarch2/src/MainController.java
@@ -0,0 +1,357 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+public class MainController implements ActionListener
+{
+    private MainGUI GUI;
+    private String sign;
+    //private String hexadecimal;
+    private String exponent;
+    // inputs
+    private String binary;
+    private String hexadecimal;
+
+    public MainController()
+    {
+
+        GUI = new MainGUI();
+        GUI.getConvertButton().addActionListener(this);
+        GUI.getClearButton().addActionListener(this);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == GUI.getConvertButton())
+        {
+            //Get input
+            sign = GUI.getsign_textField().getText(); //REMOVE since sign isnt part of our input
+            binary = GUI.getinputBinary_textField().getText(); // KEEP, add another like this but "Hexadecimal"
+            exponent = GUI.getexponent_textField().getText(); // REMOVE since the exponent isnt part of our input
+            System.out.printf("Sign: %s\n", sign);
+            System.out.printf("Binary: %s\n", binary);
+            System.out.printf("Exponent: %s\n", exponent);
+
+            //checking if sign is not + or -
+            //REMOVE since the exponent isnt part of our input
+            if (!(sign.equals("+")) && !(sign.equals("-")))
+            {
+                JOptionPane.showMessageDialog(null, "Invalid sign input! + or - only.");
+                GUI.getsign_textField().setText("");
+            }
+
+            //KEEP
+            //Maybe change the condition to binary.length()==0 && hexadecimal.length() == 0
+            //Since ours can have either of the inputs
+            if (binary.length() == 0)
+            {
+                JOptionPane.showMessageDialog(null, "Binary field is empty. Try again.");
+
+
+            }
+
+            //REMOVE since the exponent isnt part of our input
+            if (exponent.length() == 0)
+            {
+                JOptionPane.showMessageDialog(null, "Exponent field is empty. Try again.");
+
+            }
+
+            // BEFORE the normalize code below, put a function for converting hexadecimal to binary(?)
+            // https://www.geeksforgeeks.org/java-program-to-convert-hexadecimal-to-binary/
+
+            //normalize if not within range 1.0 to 2.0, if input is 0 then immediately convert (if input is binary)
+            int ctr = 0;
+            double float_binary = Double.parseDouble(binary);
+            System.out.printf("float_binary: %f\n", float_binary);
+            if(float_binary != 0)
+            {
+                if(float_binary < 1.0)
+                {
+                    while(float_binary < 1.0)
+                    {
+                        float_binary *= 10.0;
+                        ctr--;
+                    }
+                }
+
+                if(float_binary > 2.0)
+                {
+                    while(float_binary > 2.0)
+                    {
+                        float_binary /= 10.0;
+                        ctr++;
+                    }
+                }
+            }
+
+            //Dunno whats happening here
+            System.out.printf("counter(add to exponent): %d\n", ctr);
+            String binary = String.format("%.15f", float_binary);
+            int j = binary.length();
+
+            // In our case, we should extract the first character then store it sa signbit variable. Then I guess we just swap
+            // The + with 0 and - with 1 in this part below
+            //get the sign bit
+            String signbit="";
+            if(sign.equals("+"))
+            {
+                signbit = "0";
+            }
+            else
+                signbit = "1";
+            System.out.printf("signbit: %s\n", signbit);
+
+            //REMOVE(?)
+            //I guess in our case we should get the 2nd-11th digit of the binary input
+            //Then convert to decimal, then do the subtraction e'-1023
+            //Answer will be stored in 'e' or 'exponent' variable
+            // for(i=1;i<11;i++)
+            //		e_prime = binary.charAt(i)
+            //
+            // e = Integer.parseInt(e_prime,2)) - 1023 (Ex. 5 = 1028-1023)
+            int int_exponent = Integer.parseInt(exponent);
+            //add ctr to exponent after normalizing
+            int_exponent += ctr;
+            int e_prime = int_exponent + 127;
+
+
+            //Special Case 4
+            //negative infinity
+            if(int_exponent > 127 && signbit.equals("1"))
+            {
+                e_prime = 255;
+                JOptionPane.showMessageDialog(null, "Special Case: Negative Infinity");
+            }
+
+            //Special Case 5
+            //positive infinity
+            if(int_exponent > 127 && signbit.equals("0"))
+            {
+                e_prime = 255;
+                JOptionPane.showMessageDialog(null, "Special Case: Positive Infinity");
+            }
+
+            String e_binary = Integer.toBinaryString(e_prime);
+            if(binary.equals("0.000000000000000"))
+            {
+                e_binary = "00000000";
+            }
+            else
+            {
+                while(e_binary.length() < 8)
+                {
+                    e_binary = "0" + e_binary;
+                }
+            }
+            System.out.printf("e_binary: %s\n", e_binary);
+
+            System.out.printf("normalized mantissa: %s\n", binary);
+            int binaryLength = binary.length();
+            char digit;
+
+            boolean normalizable = true;
+            //check coefficient if 1
+            digit = binary.charAt(0);
+            if(digit != '1')
+            {
+                normalizable = false;
+            }
+
+            //check if decimal digits are 0 or 1
+            for(int l=2;l<binaryLength;l++)
+            {
+                digit = binary.charAt(l);
+                if(digit != '0' && digit != '1')
+                {
+                    normalizable = false;
+                }
+            }
+            if(binary.equals("0.000000000000000"))
+            {
+                normalizable = true;
+            }
+            if(normalizable == false)
+            {
+                JOptionPane.showMessageDialog(null, "Could not normalize");
+            }
+
+            //Special Case 3
+            if(int_exponent < -126)
+            {
+                JOptionPane.showMessageDialog(null, "Special Case: Denormalized");
+                while(int_exponent < -126)
+                {
+                    float_binary /= 10.0;
+                    int_exponent++;
+                }
+                e_binary = "00000000";
+                binary = String.format("%.15f", float_binary);
+                j = binary.length();
+            }
+
+            //In our case, we need to get the remaining 52 bits
+            //get the remaining 23 bits
+            String bit23 = "";
+            int k;
+            char temp;
+            String strtemp;
+            k = 2;
+            while(k < j)
+            {
+                temp = binary.charAt(k);
+                strtemp = Character.toString(temp);
+                bit23 = bit23 + strtemp;
+                k++;
+            }
+
+            int l = bit23.length();
+            if(int_exponent > 127)
+                bit23 = "00000000000000000000000";
+            else
+                while(l < 23 )
+                {
+                    bit23 = bit23 + "0";
+                    l++;
+                }
+            System.out.printf("bit23: %s\n", bit23);
+
+            String binaryOutput = "";
+            binaryOutput = signbit + e_binary + bit23;
+            System.out.printf("binaryOutput: %s\n", binaryOutput);
+
+            String str4bit_1 = "";
+            String str4bit_2 = "";
+            String str4bit_3 = "";
+            String str4bit_4 = "";
+            String str4bit_5 = "";
+            String str4bit_6 = "";
+            String str4bit_7 = "";
+            String str4bit_8 = "";
+            k=0;
+
+            //separate into 4 bits
+            while(k < 4)
+            {
+                temp = binaryOutput.charAt(k);
+                strtemp = Character.toString(temp);
+                str4bit_1 = str4bit_1 + strtemp;
+                k++;
+            }
+            while(k < 8)
+            {
+                temp = binaryOutput.charAt(k);
+                strtemp = Character.toString(temp);
+                str4bit_2= str4bit_2 + strtemp;
+                k++;
+            }
+            while(k < 12)
+            {
+                temp = binaryOutput.charAt(k);
+                strtemp = Character.toString(temp);
+                str4bit_3= str4bit_3 + strtemp;
+                k++;
+            }
+
+            while(k < 16)
+            {
+                temp = binaryOutput.charAt(k);
+                strtemp = Character.toString(temp);
+                str4bit_4= str4bit_4 + strtemp;
+                k++;
+            }
+
+            while(k < 20)
+            {
+                temp = binaryOutput.charAt(k);
+                strtemp = Character.toString(temp);
+                str4bit_5= str4bit_5 + strtemp;
+                k++;
+            }
+
+            while(k < 24)
+            {
+                temp = binaryOutput.charAt(k);
+                strtemp = Character.toString(temp);
+                str4bit_6= str4bit_6 + strtemp;
+                k++;
+            }
+
+            while(k < 28)
+            {
+                temp = binaryOutput.charAt(k);
+                strtemp = Character.toString(temp);
+                str4bit_7= str4bit_7 + strtemp;
+                k++;
+            }
+
+            while(k < 32)
+            {
+                temp = binaryOutput.charAt(k);
+                strtemp = Character.toString(temp);
+                str4bit_8= str4bit_8 + strtemp;
+                k++;
+            }
+            //add space after 4 bits
+            String finalBinary = str4bit_1 + " " + str4bit_2 + " "+ str4bit_3 + " " + str4bit_4 +" "+ str4bit_5 + " " + str4bit_6 +" "+ str4bit_7 + " " + str4bit_8;
+
+            System.out.printf("1: %s\n", str4bit_1);
+            System.out.printf("2: %s\n", str4bit_2);
+            System.out.printf("3: %s\n", str4bit_3);
+            System.out.printf("4: %s\n", str4bit_4);
+            System.out.printf("5: %s\n", str4bit_5);
+            System.out.printf("6: %s\n", str4bit_6);
+            System.out.printf("7: %s\n", str4bit_7);
+            System.out.printf("8: %s\n", str4bit_8);
+
+
+            int  int4bit_1 = Integer.parseInt(str4bit_1,2);
+            int  int4bit_2 = Integer.parseInt(str4bit_2,2);
+            int  int4bit_3 = Integer.parseInt(str4bit_3,2);
+            int  int4bit_4 = Integer.parseInt(str4bit_4,2);
+            int  int4bit_5 = Integer.parseInt(str4bit_5,2);
+            int  int4bit_6 = Integer.parseInt(str4bit_6,2);
+            int  int4bit_7 = Integer.parseInt(str4bit_7,2);
+            int  int4bit_8 = Integer.parseInt(str4bit_8,2);
+
+            System.out.printf("1: %s\n", int4bit_1);
+            System.out.printf("2: %s\n", int4bit_2);
+            System.out.printf("3: %s\n", int4bit_3);
+            System.out.printf("4: %s\n", int4bit_4);
+            System.out.printf("5: %s\n", int4bit_5);
+            System.out.printf("6: %s\n", int4bit_6);
+            System.out.printf("7: %s\n", int4bit_7);
+            System.out.printf("8: %s\n", int4bit_8);
+
+            String hex1 = Integer.toHexString(int4bit_1);
+            String hex2 = Integer.toHexString(int4bit_2);
+            String hex3 = Integer.toHexString(int4bit_3);
+            String hex4 = Integer.toHexString(int4bit_4);
+            String hex5 = Integer.toHexString(int4bit_5);
+            String hex6 = Integer.toHexString(int4bit_6);
+            String hex7 = Integer.toHexString(int4bit_7);
+            String hex8 = Integer.toHexString(int4bit_8);
+
+            String finalHex = hex1 + hex2 + hex3+ hex4 + hex5 + hex6 + hex7 + hex8;
+            finalHex = finalHex.toUpperCase();
+            System.out.printf("hex: %s\n", finalHex);
+            GUI.setBinaryOutput_textField().setText(finalBinary);
+            GUI.setHexOutput_textField().setText(finalHex);
+
+        }
+
+        else
+        {
+            //clear all text fields
+            GUI.getsign_textField().setText("");
+            GUI.getinputBinary_textField().setText("");
+            GUI.getexponent_textField().setText("");
+            GUI.setBinaryOutput_textField().setText("");
+            GUI.setHexOutput_textField().setText("");
+        }
+
+    }
+
+}
---
+++ csarch2/src/MainGUI.java
@@ -0,0 +1,197 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+import java.awt.Color;
+import javax.swing.JTextArea;
+
+public class MainGUI extends JFrame{
+
+
+    private JTextField DecimalOutput_textField;// decimal output
+    private JTextField exponent_textField;//REMOVE LATER
+    private JLabel lblNewLabel; //title ng program
+    private JTextField HexOutput_textField; //REMOVE LATER
+    private JButton ConvertButton; //fixed
+    private JButton ConvertButton2; //FP
+    private JTextField inputBinary_textField; //input for binary
+    private JTextField inputHexadecimal_textField; //input for hex
+    private JTextField sign_textField; //REMOVE LATER
+    private JButton ClearButton; //clear
+    private JButton PasteButton; //paste to notepad
+    private JLabel lblOutput; //output label
+    private JLabel lblBinary; //binary label
+    private JLabel lblHexadecimal; //hex label
+    private JLabel exp_label; //REMOVE LATER
+    private JLabel binary_Label; //REMOVE LATER
+    private JLabel sign_label; //REMOVE LATER
+
+    public MainGUI() {
+        setBounds(100, 100, 878, 663);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
+        initialize();
+        setVisible(true);
+    }
+
+
+    /**
+     * Create the application.
+     */
+
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+
+        DecimalOutput_textField = new JTextField();
+        DecimalOutput_textField.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        DecimalOutput_textField.setBounds(317, 446, 227, 32);
+        getContentPane().add(DecimalOutput_textField);
+        DecimalOutput_textField.setColumns(10);
+
+        lblNewLabel = new JLabel("IEEE-754 Binary-64 floating point translator ");
+        lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 35));
+        lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        lblNewLabel.setBounds(1, 10, 666, 62);
+        getContentPane().add(lblNewLabel);
+
+        ConvertButton = new JButton("Fixed");
+        ConvertButton.setForeground(Color.BLACK);
+        ConvertButton.setFont(new Font("Calibri", Font.PLAIN, 15));
+        ConvertButton.setBounds(340, 320, 85, 32);
+        getContentPane().add(ConvertButton);
+
+        ConvertButton2 = new JButton("FP");
+        ConvertButton2.setForeground(Color.BLACK);
+        ConvertButton2.setFont(new Font("Calibri", Font.PLAIN, 15));
+        ConvertButton2.setBounds(450, 320, 85, 32);
+        getContentPane().add(ConvertButton2);
+
+        lblOutput = new JLabel("Output");
+        lblOutput.setFont(new Font("Calibri", Font.PLAIN, 20));
+        lblOutput.setBounds(410, 403, 85, 32);
+        getContentPane().add(lblOutput);
+
+        lblBinary = new JLabel("Binary");
+        lblBinary.setFont(new Font("Calibri", Font.PLAIN, 20));
+        lblBinary.setBounds(409, 234, 85, 32);
+        getContentPane().add(lblBinary);
+
+        lblHexadecimal = new JLabel("Hexadecimal");
+        lblHexadecimal.setFont(new Font("Calibri", Font.PLAIN, 20));
+        lblHexadecimal.setBounds(384, 153, 133, 32);
+        getContentPane().add(lblHexadecimal);
+
+        ClearButton = new JButton("Clear");
+        ClearButton.setForeground(Color.BLACK);
+        ClearButton.setFont(new Font("Calibri", Font.PLAIN, 15));
+        ClearButton.setBounds(681, 553, 85, 32);
+        getContentPane().add(ClearButton);
+
+        PasteButton = new JButton("Paste");
+        PasteButton.setForeground(Color.BLACK);
+        PasteButton.setFont(new Font("Calibri", Font.PLAIN, 15));
+        PasteButton.setBounds(591, 553, 85, 32);
+        getContentPane().add(PasteButton);
+
+        inputBinary_textField = new JTextField();
+        inputBinary_textField.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        inputBinary_textField.setColumns(10);
+        inputBinary_textField.setBounds(199, 264, 463, 32);
+        getContentPane().add(inputBinary_textField);
+
+        //ADDITIONAL
+        inputHexadecimal_textField = new JTextField();
+        inputHexadecimal_textField.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        inputHexadecimal_textField.setColumns(10);
+        inputHexadecimal_textField.setBounds(199, 184, 463, 32);
+        getContentPane().add(inputHexadecimal_textField);
+        //ADDITIONAL
+
+    }
+
+    //REMOVE
+    public JTextField getsign_textField()
+    {
+        return sign_textField;
+    }
+
+    //REMOVE
+    public void setsign_textField(JTextField sign_textField)
+    {
+        this.sign_textField = sign_textField;
+    }
+
+    //SET FOR BINARY INPUT
+    public void setinputBinary_textField(JTextField inputBinary_textField)
+    {
+        this.inputBinary_textField = inputBinary_textField;
+    }
+
+    //SET FOR HEX INPUT
+    public void setInputHexadecimal_textField(JTextField inputHexadecimal_textField)
+    {
+        this.inputHexadecimal_textField = inputHexadecimal_textField;
+    }
+
+    //REMOVE
+    public void setexponent_textField(JTextField exponent_textField)
+    {
+        this.exponent_textField = exponent_textField;
+    }
+
+    //GET for both Binary and Hex input
+    public JTextField getinputBinary_textField()
+    {
+        return inputBinary_textField;
+    }
+    public JTextField getInputHexadecimal_textField()
+    {
+        return inputHexadecimal_textField;
+    }
+
+    //REMOVE
+    public JTextField getexponent_textField()
+    {
+        return exponent_textField;
+    }
+
+    //REMOVE since we only need 1 output text field
+    public JTextField setHexOutput_textField()
+    {
+        return HexOutput_textField;
+    }
+
+    public JTextField setBinaryOutput_textField() //setDecimalOutput_textfield() change later in maincontroller
+    {
+        return DecimalOutput_textField;
+    }
+    public JButton getConvertButton()
+    {
+        return ConvertButton;
+    }
+
+    public JButton getConvertButton2()
+    {
+        return ConvertButton2;
+    }
+
+    public JButton getClearButton()
+    {
+        return ClearButton;
+    }
+
+    public JButton getPasteButton()
+    {
+        return PasteButton;
+    }
+}
